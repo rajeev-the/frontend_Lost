cac63d0f3733ab6bb33299c95250a814e0ca0bc5
rajeev-the <146732212+rajeev-the@users.noreply.github.com>
2025-01-17 20:58:37 +0530

    Update LostPage.jsx

--- src/Pages/LostPage.jsx
+++ src/Pages/LostPage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState ,useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Nav from '../components/Nav';
+import { useNavigate } from 'react-router-dom'
 
 const LostPage = () => {
 
